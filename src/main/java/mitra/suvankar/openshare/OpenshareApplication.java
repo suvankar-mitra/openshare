2a377c05ebcead815acdda5e7227c16897007e8b
Suvankar Mitra <16486734+suvankar-mitra@users.noreply.github.com>
2024-07-13 15:00:00 +0530

    Initial commit

---
+++ src/main/java/mitra/suvankar/openshare/OpenshareApplication.java
@@ -0,0 +1,13 @@
+package mitra.suvankar.openshare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mitra/suvankar/openshare/OpenshareApplicationTests.java
@@ -0,0 +1,13 @@
+package mitra.suvankar.openshare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenshareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
